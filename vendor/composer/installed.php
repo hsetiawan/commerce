cdee4386707dbdce7605ffede52a2c868051acca
hsetiawan <hendset.ti04@gmail.com>
2020-12-25 06:23:15 +0700

    add composer files by update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5d5f50c9b469e6f0af7a4f05ef5761fd54a28183',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5d5f50c9b469e6f0af7a4f05ef5761fd54a28183',
+    ),
+  ),
+);
